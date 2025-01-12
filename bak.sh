0a918560cbe945c96511515aed527214038498e6
am2b <20837051+am2b@users.noreply.github.com>
2025-01-12 13:24:55 +0800

    update bak.sh:usage

--- bak.sh
+++ bak.sh
@@ -1,31 +1,63 @@
 #!/usr/bin/env bash
 
 #=tools
-#@create a file[.sh].bak from file[.sh],or a file[.sh] from file[.sh].bak
-
+#@create a file.bak from file,or create a file from file.bak
 #@usage:
-#@bak.sh file.sh -> file.sh.bak
-#@bak.sh file.sh.bak -> file.sh
+#@bak.sh file -> file.bak
+#@bak.sh file.bak -> file
 
-if (("${#}" != 1)); then
+usage() {
+    local script
+    script=$(basename "$0")
+    echo "usage:"
+    echo "$script file"
+    echo "$script file.bak"
     exit 1
-fi
+}
 
-if ! [ -f "${1}" ]; then
-    exit 1
-fi
+check_parameters() {
+    if (("$#" != 1)); then
+        usage
+    fi
+}
 
-filename=$(basename "${1}")
-suffix="${filename##*.}"
+process_opts() {
+    while getopts ":h" opt; do
+        case $opt in
+        h)
+            usage
+            ;;
+        *)
+            echo "error:unsupported option -$opt"
+            usage
+            ;;
+        esac
+    done
+}
 
-if [[ "${suffix}" != 'bak' ]]; then
-    new_filename="${filename}".bak
-else
-    #filename without bak
-    new_filename="${filename%.*}"
-fi
+main() {
+    check_parameters "${@}"
+    process_opts "${@}"
+    shift $((OPTIND - 1))
 
-dir=$(dirname "${1}")
-cd "${dir}" || exit 1
+    if ! [ -f "${1}" ]; then
+        usage
+    fi
 
-cp "${filename}" "${new_filename}"
+    filename=$(basename "${1}")
+    suffix="${filename##*.}"
+
+    if [[ "${suffix}" != 'bak' ]]; then
+        new_filename="${filename}".bak
+    else
+        #filename without bak
+        new_filename="${filename%.*}"
+    fi
+
+    dir=$(dirname "${1}")
+    cd "${dir}" || exit 1
+
+    cp "${filename}" "${new_filename}"
+}
+
+main "${@}"
