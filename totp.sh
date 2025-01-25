503ba8509e5c0422f6dbb3f9de89f020e71dc242
am2b <20837051+am2b@users.noreply.github.com>
2025-01-25 19:35:53 +0800

    modify totp.sh:rename totp_url to totp_uri

--- totp.sh
+++ totp.sh
@@ -3,13 +3,13 @@
 #=password
 #@generate totp(Time-Based One-Time Password)
 #@usage:
-#@totp.sh "totp_url"
+#@totp.sh "totp_uri"
 
 usage() {
     local script
     script=$(basename "$0")
     echo "usage:"
-    echo "$script \"totp_link\""
+    echo "$script \"totp_uri\""
     exit 1
 }
 
@@ -42,15 +42,15 @@ check_dependent_tools() {
 }
 
 generate_totp() {
-    local totp_url="${1}"
+    local totp_uri="${1}"
 
     #提取secret的值(通常使用Base32编码)(label和issuer仅用于标识服务,不影响TOTP的计算)
-    if [[ $totp_url =~ secret=([^&]+) ]]; then
+    if [[ $totp_uri =~ secret=([^&]+) ]]; then
         local secret="${BASH_REMATCH[1]}"
         #生成一次性密码
         oathtool --base32 --totp "${secret}"
     else
-        echo "error:invalid totp link"
+        echo "error:invalid totp uri"
         return 1
     fi
 }
@@ -62,8 +62,8 @@ main() {
 
     check_dependent_tools
 
-    local totp_url="${1}"
-    otp=$(generate_totp "${totp_url}")
+    local totp_uri="${1}"
+    otp=$(generate_totp "${totp_uri}")
     echo "${otp}"
 }
 
