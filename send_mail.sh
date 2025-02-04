a431ed2526b3aacdc9599aa20bd78704fc0f6c90
am2b <20837051+am2b@users.noreply.github.com>
2025-02-04 21:39:52 +0800

    update send_mail.sh:add usage

--- send_mail.sh
+++ send_mail.sh
@@ -1,32 +1,77 @@
 #!/usr/bin/env bash
 
 #=tools
-#@usage:script.sh "subject" "body" recipient
+#@usage:
+#@send_mail.sh "subject" "body" recipient
 
-# 检查是否安装了msmtp
-if ! command -v msmtp &> /dev/null; then
-    echo "msmtp未安装,正在通过Homebrew安装..."
-    
-    # 检查是否安装了Homebrew
-    if ! command -v brew &> /dev/null; then
-        echo "Homebrew未安装,请先安装Homebrew"
-        exit 1
+usage() {
+    local script
+    script=$(basename "$0")
+    echo "usage:"
+    echo "$script \"subject\" \"body\" \"recipient\""
+    exit 1
+}
+
+check_parameters() {
+    if (("$#" != 3)); then
+        usage
     fi
-    
-    # 使用Homebrew安装msmtp
-    brew install msmtp
-    
-    # 检查是否安装成功
-    if ! command -v msmtp &> /dev/null; then
-        echo "msmtp安装失败,请检查Homebrew设置"
-        exit 1
-    else
-        echo "msmtp安装成功"
+}
+
+check_tools() {
+    #检查是否安装了msmtp
+    if ! command -v msmtp &>/dev/null; then
+        echo "msmtp未安装,正在通过Homebrew安装..."
+
+        # 检查是否安装了Homebrew
+        if ! command -v brew &>/dev/null; then
+            echo "Homebrew未安装,请先安装Homebrew"
+            exit 1
+        fi
+
+        #使用Homebrew安装msmtp
+        brew install msmtp
+
+        #检查是否安装成功
+        if ! command -v msmtp &>/dev/null; then
+            echo "msmtp安装失败,请检查Homebrew设置"
+            exit 1
+        else
+            echo "msmtp安装成功"
+        fi
     fi
-fi
+}
 
-subject=$1
-body=$2
-recipient=$3
+process_opts() {
+    while getopts ":h" opt; do
+        case $opt in
+        h)
+            usage
+            ;;
+        *)
+            echo "error:unsupported option -$opt"
+            usage
+            ;;
+        esac
+    done
+}
 
-nohup sh -c "printf 'To: %s\nSubject: %s\n\n%s\n' \"$recipient\" \"$subject\" \"$body\" | msmtp \"$recipient\"" > /dev/null 2>&1 &
+do_send() {
+    subject=$1
+    body=$2
+    recipient=$3
+
+    nohup sh -c "printf 'To: %s\nSubject: %s\n\n%s\n' \"$recipient\" \"$subject\" \"$body\" | msmtp \"$recipient\"" >/dev/null 2>&1 &
+}
+
+main() {
+    check_parameters "${@}"
+    process_opts "${@}"
+    shift $((OPTIND - 1))
+
+    check_tools
+
+    do_send "${@}"
+}
+
+main "${@}"
